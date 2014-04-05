fe4fc4e87f50f43e362aeb0b75a4cbdebfd87d7a
sonnm56 <sonnm_56@vnu.edu.vn>
2014-04-06 06:44:42 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestBlog::Application.config.secret_key_base = 'ffbda009555504a46e420de84feb1e2f10851dd023b8135db9503c01ef112fc80518ae563440adaab0e2cb6ff703c4ad1762e0ecc71cd6b4f2a68db8c2a4416b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,16 @@
+# Be sure to restart your server when you modify this file.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+TestBlog::Application.config.session_store :cookie_store, key: '_test_blog_session'
